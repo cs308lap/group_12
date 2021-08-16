b1f52ea5db88699a4f159d70d2bae8169d58d524
Gaurav <b19083@students.iitmandi.ac.in>
2021-08-16 15:56:31 +0530

    initial commit

--- factorial.cpp
+++ factorial.cpp
@@ -1,6 +1,12 @@
 #include "functions.h"
 
 int factorial(int n){
+	if(n<0){
+		int x;
+		cout<<"Please enter valid non-negative integer: ";
+		cin>>x;
+		return factorial(x);
+	}
     if(n!=1){
 	return(n * factorial(n-1));
     }
