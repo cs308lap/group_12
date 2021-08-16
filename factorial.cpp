4e31e947c1b8baf4a5b7d258d2a45dd89d2eab97
Gaurav <b19083@students.iitmandi.ac.in>
2021-08-16 16:02:53 +0530

    full commit

--- factorial.cpp
+++ factorial.cpp
@@ -1,14 +1,18 @@
 #include "functions.h"
 
+// function to calculate factorial of a number
 int factorial(int n){
-	if(n<0){
-		int x;
-		cout<<"Please enter valid non-negative integer: ";
-		cin>>x;
-		return factorial(x);
-	}
+
+    // if a negative number is entered, the user is asked to re-enter a positive number
+    if (n<0){
+        int x;
+        cout<<"enter a positive number:\n";
+        cin>>x;
+        return factorial(x);
+    }
+
     if(n!=1){
-	return(n * factorial(n-1));
+	return(n * factorial(n-1)); //recursively calling the factorial function.
     }
     else return 1;
 }
--- hello.cpp
+++ hello.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include "functions.h"
 using namespace std;
+
+//Just to test
 void print_hello(){
    cout<<"Hello World!";
 }
