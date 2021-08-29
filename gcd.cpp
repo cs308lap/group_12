1190865e5bdf05ae1390565ee781e03c9d0e3578
Gaurav Sahitya <gauravsahitya108@gmail.com>
2021-08-30 00:39:02 +0530

    conflicting commit

--- gcd.cpp
+++ gcd.cpp
@@ -6,6 +6,7 @@ int gcd(int x, int y)
 {
 	int ans;
 	
+    //This statement is added to give rise to conflicts
     for(int i=1; i<=min(x,y);i++){
         if((x%i==0) && (y%i)==0){
             ans=i;
